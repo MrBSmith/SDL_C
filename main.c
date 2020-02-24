5ded912068e593dffd6e2ff324284d7d5a038ec7
Hugo Baba <Hugobarette@gmail.com>
2020-02-24 13:14:22 +0100

    Question 6

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h> // Include the SDL library
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) // Initialisation de la SDL
+    {
+       printf("Erreur d'initialisation de la SDL : %s", SDL_GetError()); // Affiche un message d'erreur si l'initialisation de la SDL a échouée
+       return EXIT_FAILURE; // On quitte le programme si l'ouverture de la SDL a échouée
+    }
+
+    SDL_Quit(); // On quitte la SDL avant de quitter le programme
     return 0;
 }
